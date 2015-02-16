1e5a84947b8e5d6761fe1ad6410d5b3fef5763cb
Alexander Sapozhnikov <shoorick@mail.ru>
2015-02-16 20:05:27 +0500

    Durations added to output

--- formatters/sum_notes.rb
+++ formatters/sum_notes.rb
@@ -1,9 +1,28 @@
-class Timetrap::Formatters::SumNotes
-  def initialize(entries)
-    @entries = entries
-  end
+module Timetrap
+  module Formatters
+    class SumNotes
+      attr_accessor :output
+      include Timetrap::Helpers
 
-  def output
-    @entries.map{|entry| entry[:note]}.join("\n")
+      def initialize(entries)
+        self.output = ''
+        sheets = entries.inject({}) do |h, e|
+          h[e.sheet] ||= []
+          h[e.sheet] << e
+          h
+        end
+        
+        sheets.keys.sort.each do |sheet|
+          self.output <<  "Timesheet: #{sheet}\n"
+          sheets[sheet].each_with_index do |e, i|
+            #from_current_day << e
+            self.output <<  "%10s    %s\n" % [
+              format_duration(e.duration),
+              e.note
+            ]
+          end
+        end
+      end
+    end
   end
 end
